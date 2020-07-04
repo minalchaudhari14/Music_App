6ef6b3b629d3551508abc09cb6d37cb764a76909
Chaudhari minal <54309507+minalchaudhari14@users.noreply.github.com>
2020-07-04 17:16:59 +0530

    Update main.js

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ let track_list = [
     path: "music/Gilehriyaan.mp3"
   },
    {
-    name: "Inkem Inkem",
+    name: "Inkem Kaavaale",
     artist: "Sid Sriram",
     image: "img/img4.jpg",
     path: "music/Inkem Inkem Inkem Kaavaale.mp3",
